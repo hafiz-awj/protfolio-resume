51f10324f7e846a4b49c136f90f85b5098d659bf
hafiz-awj <hafizaliawj@gmail.com>
2023-09-19 12:20:11 +0500

    tailwind added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="bg-black w-10">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
